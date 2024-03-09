7b26304324a83c39eb1aa6135a388c06f8def5c5
Bakry Rihawi <bakry29bbb@gmail.com>
2024-03-09 14:54:26 +0300

    Adding  I2C files.

---
+++ H2BR1/H2BR1_i2c.c
@@ -0,0 +1,34 @@
+/*
+ BitzOS (BOS) V0.3.0 - Copyright (C) 2017-2024 Hexabitz
+ All rights reserved
+
+ File Name     : H05R0_i2c.c
+ Description   : This file provides code for the configuration
+ of the I2C instances.
+
+ */
+/* Includes ------------------------------------------------------------------*/
+#include "H2BR1_i2c.h"
+
+/* USER CODE BEGIN 0 */
+
+
+
+/* I2C2 init function */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ H2BR1/H2BR1_i2c.h
@@ -0,0 +1,41 @@
+/*
+ BitzOS (BOS) V0.3.0 - Copyright (C) 2017-2024 Hexabitz
+ All rights reserved
+
+ File Name     : H05R0_i2c.h
+ Description   :This file contains all the functions prototypes for
+ the i2c
+
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __I2C_H__
+#define __I2C_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "BOS.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __I2C_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
