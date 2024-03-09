c2570cb569c8ae10b6bdddf3492a76bb99c88210
Bakry Rihawi <bakry29bbb@gmail.com>
2024-03-09 19:59:11 +0300

    Adding i2C configuration.

--- H2BR1/H2BR1_i2c.c
+++ H2BR1/H2BR1_i2c.c
@@ -12,23 +12,92 @@
 
 /* USER CODE BEGIN 0 */
 
+/* USER CODE END 0 */
 
+I2C_HandleTypeDef hi2c2;
 
 /* I2C2 init function */
+void MX_I2C2_Init(void)
+{
+
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.Timing = 0x20303E5D;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  HAL_I2C_Init(&hi2c2);
+
+  /** Configure Analogue filter
+  */
+  HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE);
+  /** Configure Digital filter
+  */
+  HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0);
 
 
+}
 
+void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
+{
 
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(i2cHandle->Instance==I2C2)
+  {
+  /* USER CODE BEGIN I2C2_MspInit 0 */
 
+  /* USER CODE END I2C2_MspInit 0 */
 
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C2 GPIO Configuration
+    PB3     ------> I2C2_SCL
+    PB4     ------> I2C2_SDA
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF8_I2C2;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+    /* I2C2 clock enable */
+    __HAL_RCC_I2C2_CLK_ENABLE();
+  /* USER CODE BEGIN I2C2_MspInit 1 */
 
+  /* USER CODE END I2C2_MspInit 1 */
+  }
+}
 
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef* i2cHandle)
+{
 
+  if(i2cHandle->Instance==I2C2)
+  {
+  /* USER CODE BEGIN I2C2_MspDeInit 0 */
 
+  /* USER CODE END I2C2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C2_CLK_DISABLE();
 
+    /**I2C2 GPIO Configuration
+    PB3     ------> I2C2_SCL
+    PB4     ------> I2C2_SDA
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_3);
 
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_4);
 
+  /* USER CODE BEGIN I2C2_MspDeInit 1 */
 
+  /* USER CODE END I2C2_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- H2BR1/H2BR1_i2c.h
+++ H2BR1/H2BR1_i2c.h
@@ -22,16 +22,17 @@ extern "C" {
 
 /* USER CODE END Includes */
 
+extern I2C_HandleTypeDef hi2c2;
 
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
 
+void MX_I2C2_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 
 /* USER CODE END Prototypes */
-
 #ifdef __cplusplus
 }
 #endif
